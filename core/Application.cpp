1315ba0f1706fef09ce21c6c502277bf82f7ddd4
Sergey Lyubimov <lysevi@gmail.com>
2014-12-09 19:25:34 +0300

    core: build fix.

--- core/Application.cpp
+++ core/Application.cpp
@@ -43,7 +43,7 @@ int Application::OnExecute() {
 
 bool Application::OnInit() {
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        std::cerr << "Init error" <<SDL_GetError()<<endl;
+        std::cerr << "Init error" <<SDL_GetError()<<std::endl;
         return false;
     }
 
